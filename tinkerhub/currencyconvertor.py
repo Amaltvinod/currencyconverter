ecb6d664cf9c2e475692a1945622875708d28245
Amaltvinod <amaltvinod5@gmail.com>
2021-08-15 21:32:54 +0530

    code changes

--- tinkerhub/currencyconvertor.py
+++ tinkerhub/currencyconvertor.py
@@ -6,12 +6,13 @@ window.title("CurrencyConverter")
 window.geometry("700x700")
 
 
-photo = Image.open("currency.jpg")
-img = ImageTk.PhotoImage(photo)
+img = ImageTk.PhotoImage(Image.open("images.png"))
 label = Label(window,image=img)
 label.image = img
 label.place(x=200,y=0)
 
+convert_button = Button(window,text="CONVERT",fg="white",bg="grey",borderwidth=2)
+convert_button.place(x=550,y=180)
 
 options = [
             "US Dollar (USD)",
@@ -26,27 +27,27 @@ options = [
             "Swedish Krona(SEK)"
         ]
 
-label1 = Label(window,text = "Amount : ").place(x=130,y=180)
-amount = Entry(window)
-amount.place(x=230,y=180)
+label1 = Label(window,text = "Amount : ").place(x=250,y=180)
+amount = Entry(window,borderwidth=5)
+amount.place(x=400,y=180)
 
-label2 = Label(window,text = "Convert From : ").place(x=130,y=220)
+label2 = Label(window,text = "Convert From : ").place(x=250,y=220)
 clicked1 = StringVar()
 clicked1.set("US Dollar (USD)")
 dropfrom = OptionMenu(window,clicked1,*options)
 dropfrom.pack()
-dropfrom.place(x=230,y=215)
+dropfrom.place(x=400,y=215)
 
-label3 = Label(window,text = "Convert To : ").place(x=130,y=270)
+label3 = Label(window,text = "Convert To : ").place(x=250,y=270)
 clicked2 = StringVar()
 clicked2.set("Indian Rupee (RUP)")
 dropto = OptionMenu(window,clicked2,*options)
 dropto.pack()
-dropto.place(x=230,y=265)
+dropto.place(x=400,y=265)
 
-label4 = Label(window,text = "Converted Amount : ").place(x=130,y=315)
-amount = Entry(window)
-amount.place(x=240,y=317)
+label4 = Label(window,text = "Converted Amount : ").place(x=250,y=315)
+amount = Entry(window,borderwidth=5)
+amount.place(x=400,y=317)
 
 
 window.mainloop()
